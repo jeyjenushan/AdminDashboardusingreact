6aa5084bcc47fe124aafcf1f2ef40054577012c5
Jeyaruban jenushan <jjenushan550@gmail.com>
2025-01-05 11:29:54 +0530

    newly added charts,header bar

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import DashboardStatsGrid from './DashboardStatsGrid'
+import TransactionChart from './TransactionChart'
+import BuyerProfilePieChart from './BuyerProfilePieChart'
+import RecentOrders from './RecentOrders'
+import PopularProducts from './PopularProducts'
 
 function Dashboard() {
   return (
-    <div>
-      dashboard
+    <div className='flex flex-col gap-4'>
+ <DashboardStatsGrid/>
+ <div className='flex flex-row gap-4 w-full'>
+ <TransactionChart/>
+ <BuyerProfilePieChart/>
+ </div>
+ <div className='flex flex-row gap-4 w-full'>
+<RecentOrders/>
+<PopularProducts/>
+ </div>
+
     </div>
   )
 }
--- src/Components/shared/Layout.jsx
+++ src/Components/shared/Layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Sidebar from './Sidebar'
+import Header from './Header'
 
 function Layout() {
   return (
@@ -8,8 +9,11 @@ function Layout() {
     overflow-hidden
     '>
         <Sidebar/>
+        <div className='flex-1'>
+<Header/>
         <div className='p-4'>
           {<Outlet/>}
+        </div>
         </div>
       
     </div>
